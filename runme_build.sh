74956ee0fcac0eafd9d2ff2d1b4efd3ac52085af
Polin <polinwong@yandex.com>
2024-05-08 16:11:45 +0800

    ...

--- runme_build.sh
+++ runme_build.sh
@@ -5,7 +5,9 @@ do_cmd(){
 	[[ $? -ne 0 ]] && echo "do_cmd failed" && exit
 }
 do_cmd cmake -DCMAKE_CUDA_COMPILER=/usr/local/cuda-12.4/bin/nvcc -DCMAKE_CXX_COMPILER=/usr/bin/g++-13 -DBUILD_SHARED_LIBS=ON -B build .
-do_cmd make -C build -j faiss
-do_cmd make -C build -j swigfaiss
-do_cmd (cd build/faiss/python && python setup.py install)
+do_cmd make -C build -j$(nproc) faiss
+do_cmd make -C build -j$(nproc) swigfaiss
+
+do_cmd cd build/faiss/python
+do_cmd python setup.py install
 do_cmd sudo make -C build install
