edf90d397d828ef50f7991b3853c464ce606f5b8
Your Name <you@example.com>
2024-05-08 16:47:02 +0800

    ...

--- install_apt_packages.sh
+++ install_apt_packages.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 检查是否以root权限运行
-if [ "$(id -u)" != "0" ]; then
-   echo "此脚本必须以root权限运行" 1>&2
-   exit 1
-fi
+#if [ "$(id -u)" != "0" ]; then
+#   echo "此脚本必须以root权限运行" 1>&2
+#   exit 1
+#fi
 
 # 从apt_requirements.txt文件中读取软件包名称并安装
 while IFS= read -r pkg
--- runme_build.sh
+++ runme_build.sh
@@ -4,10 +4,17 @@ do_cmd(){
 	$@
 	[[ $? -ne 0 ]] && echo "do_cmd failed" && exit
 }
-do_cmd cmake -DCMAKE_CUDA_COMPILER=/usr/local/cuda-12.4/bin/nvcc -DCMAKE_CXX_COMPILER=/usr/bin/g++-13 -DBUILD_SHARED_LIBS=ON -B build .
-do_cmd make -C build -j$(nproc) faiss
-do_cmd make -C build -j$(nproc) swigfaiss
 
+do_jobs(){
+    do_cmd cmake -DCMAKE_CUDA_COMPILER=/usr/local/cuda-12.4/bin/nvcc -DCMAKE_CXX_COMPILER=/usr/bin/g++-13 -DBUILD_SHARED_LIBS=ON -B build .
+    do_cmd make -C build -j$(nproc) faiss
+    do_cmd make -C build -j$(nproc) swigfaiss
+}
+
+do_jobs
+oldpwd=$PWD
 do_cmd cd build/faiss/python
 do_cmd python setup.py install
+do_cmd cd $oldpwd
 do_cmd sudo make -C build install
+echo "done"
